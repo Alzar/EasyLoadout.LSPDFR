df4e3130d5032e1eea1d4eb01e603a1f7afd23be
Alzar <srichardson97402@hotmail.com>
2019-02-25 16:23:25 -0800

    Initial Commit

---
+++ EntryPoint.cs
@@ -0,0 +1,29 @@
+﻿/*
+ *	Developed By: Alzar
+ *	Name: Easy Loadout
+ *	Dependent: Rage Plugin Hook & LSPDFR
+ *	Released On: GitHub & LSPDFR
+ */
+[assembly: Rage.Attributes.Plugin("Easy Loadout LSPDFR", Author = "sr7066", Description = "Small LSPDFR plugin that triggers loadout give when going on & off duty", SupportUrl = "https://github.com/Alzar/EasyLoadout.LSPDFR/issues")]
+
+namespace EasyLoadoutLSPDFR {
+	using LSPD_First_Response.Mod.API;
+	using EasyLoadout.Core.Utils;
+
+	public class Main : Plugin {
+
+		public override void Initialize() {
+			Functions.OnOnDutyStateChanged += this.DutyStateChange;
+
+			string CurrentVersion = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Version}";
+			Updater.VersionCheck("LSPDFR", "https://raw.githubusercontent.com/Alzar/EasyLoadout.LSPDFR/master/core-latest.txt", CurrentVersion);
+		}
+
+		public void DutyStateChange(bool OnDuty) {
+			LoadoutHandler.LSPDFRLoadout(OnDuty);
+		}
+
+		public override void Finally() {
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# Easy Loadout (LSPDFR)
+
+This is a small LSPDFR plugin that will automatically activate Easy Loadout when you go on-duty with LSPDFR and give you your default loadout.
+
+## Requirements
+
+* Rage Plugin Hook (Developed and tested on RPH v0.62.1216.14731)
+* LSPD First Response (Developed and tested on LSPDFR v0.3.1)
+* RageNativeUI (Developed and tested on RageNatuveUI v1.6.3)
+* EasyLoadout (Developed and tested on EasyLoadout v1.10.1.70)
+
+NOTE: This does require you have Easy Loadout install, but does not require that it is loaded when you go on-duty to work.
+
+#### LSPDFR Release
+
+* [LSPDFR Release Page](http://www.lcpdfr.com/files/file/17259-easy-loadout/)
+
+
+### NOTE FOR MODDERS
+
+All of my GTA 5 mods are open source and if you're a modder that's wanting to either update an old mod that I've stopped updating or put your own spin on it feel free to use my code! I just ask that you simply give me some sort of credit somewhere on your release page!
